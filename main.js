d659c60c0396dd605c912bbbe84a0c91137c18bf
angeladavis2010 <angeladavis2010@yahoo.com>
2019-04-10 14:19:11 -0400

    My Contact List

---
+++ main.js
@@ -0,0 +1,25 @@
+//Get user input
+var filter= document.querySelector("#filter")
+
+
+// Add event listner to listen user input
+filter.addEventListener("keyup", filterNames)
+
+// filter out list depends on the user input
+function filterNames(){
+   // Get value of user input 
+   var value = filter.value;
+
+   // Get all names in list
+   var names = document.querySelectorAll("li a");
+  
+    //Loop over names
+    for(var i=0;i<names.length;i++){
+        // compare names
+        if(names[i].innerText.toUpperCase().includes(value.toUpperCase())){
+            names[i].parentElement.style.display ="block"
+        }else{
+            names[i].parentElement.style.display ="none"
+        }
+    }
+}
